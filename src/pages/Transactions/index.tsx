3f068b1c505aa773eae57af558cf54dfc8b8d676
gabriel-corto <digital.gabriel2006@gmail.com>
2024-11-30 00:04:37 +0100

    feat: create searchForm component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,43 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
+import { PriceHightLight, TransactionsContainer, TransactionsTable } from "./style";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <SearchForm />
+
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de Sites</td>
+              <td>
+                <PriceHightLight variant="income">
+                  AOA 12.000,00
+                </PriceHightLight>
+              </td>
+              <td>Venda</td>
+              <td>13/11/2024</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <PriceHightLight variant="outcome">
+                  - AOA 12.000,00
+                </PriceHightLight>
+              </td>
+              <td>Alimentação</td>
+              <td>13/11/2024</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
