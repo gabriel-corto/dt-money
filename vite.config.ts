305a8ac349b86faecd3970bcc5c297af19b515b5
gabriel-corto <digital.gabriel2006@gmail.com>
2025-04-04 03:01:32 +0100

    feat: add default Vitest configuration to Vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { configDefaults } from "vitest/config"
 export default defineConfig({
   plugins: [react()],
   test: {
+    ...configDefaults,
     environment: "jsdom",
     globals: true,
     setupFiles: "./src/tests/setup.ts",
